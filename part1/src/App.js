d5fb2a589b547bbada02299ffc5035171c281b50
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2021-08-09 18:25:21 -0400

    feat(part1): Add App to project

---
+++ part1/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to React App</h1>
+      <h3>Date : {new Date().toDateString()}</h3>
+    </div>
+  );
+};
+
+export default App;
