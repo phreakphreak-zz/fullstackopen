c1479384f213505d29ce197e27a200f4bc5b2795
Michael Zabala (Mike) <wonkaphreak@gmail.com>
2021-08-09 23:33:14 -0400

    feat(part1): Create service to get courses

---
+++ part1/src/services/getCourses.js
@@ -0,0 +1,42 @@
+const Courses = [
+  {
+    id: 1,
+    title: 'Half Stack application development',
+    parts: [
+      {
+        id: 1,
+        title: 'Introduction to the Half Stack application development',
+        exercises: 10,
+      },
+      {
+        id: 2,
+        title: 'Using props to pass data',
+        exercises: 7,
+      },
+      {
+        id: 3,
+        title: 'State of a component',
+        exercises: 14,
+      },
+
+      {
+        id: 4,
+        title: 'React Router',
+        exercises: 16,
+      },
+      {
+        id: 5,
+        title: 'Using the router',
+        exercises: 16,
+      },
+
+      {
+        id: 6,
+        title: 'Using hooks',
+        exercises: 9,
+      },
+    ],
+  },
+];
+
+export default Courses;
